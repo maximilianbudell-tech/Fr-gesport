f53174f57ebcba5baf70442dcf1d9ef7961e9ce0
Maximilian Budell <maximilian.budell@elev.ga.ntig.se>
2026-09-17 15:56:53 +0200

    kgktc

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Runtime.CompilerServices;
+
+String Svar1 = "";
+
+int Poäng = 5;
+Console.WriteLine("Startpoäng = 1");
+Console.WriteLine("Vad är Sveriges Huvudstad?");
+Console.WriteLine("A) Stockholm, B) Göteborg, C) Malmö");
+Svar1 = Console.ReadLine();
+if (Svar1 == "A")
+{
+    Console.WriteLine("Du har rätt! +5 poäng");
+    Poäng++;
+}
+else
+{
+    Console.WriteLine("Fel!");
+}
+
+String Svar2 = "";
+
+Console.WriteLine("Vilka länder ingår i Scandinavia?");
+Console.WriteLine("A) Sverige, Danmark, Finland. B) Sverige, Finland, Norge. C) Svergie, Danmark, Norge");
+Svar2 = Console.ReadLine();
+if (Svar2 == "C")
+{
+    Console.WriteLine("Du har rätt! +5 poäng");
+    Poäng++;
+}
+else
+{
+    C
+}
+Console.WriteLine(Poäng);
+Console.ReadLine();
+ 
+ 
+
